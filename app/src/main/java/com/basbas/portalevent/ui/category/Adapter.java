e17727d066d54722fbb2eb6fa69e1775e4a11eea
masudinn <fachrudin.2000@students.amikom.ac.id>
2020-05-27 11:14:12 +0700

    add Adapter at UI/category

---
+++ app/src/main/java/com/basbas/portalevent/ui/category/Adapter.java
@@ -0,0 +1,39 @@
+package com.basbas.portalevent.ui.category;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class Adapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentArrayList;
+    private ArrayList<String> arrayList;
+
+    public Adapter(@NonNull FragmentManager fm, ArrayList<Fragment> fragmentArrayList, ArrayList<String> arrayList) {
+        super(fm);
+        this.fragmentArrayList = fragmentArrayList;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return arrayList.get(position);
+    }
+}
