e5df130e278cdece18cf9630c8ca0052724b7a58
masudinn <fachrudin.2000@students.amikom.ac.id>
2020-06-02 16:29:42 +0700

    add intent keranjang

---
+++ app/src/main/java/com/basbas/portalevent/ui/slideshow/KeranjangActivity.java
@@ -0,0 +1,16 @@
+package com.basbas.portalevent.ui.slideshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.basbas.portalevent.R;
+
+public class KeranjangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keranjang);
+    }
+}
--- app/src/main/java/com/basbas/portalevent/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/basbas/portalevent/ui/slideshow/SlideshowFragment.java
@@ -1,5 +1,6 @@
 package com.basbas.portalevent.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.basbas.portalevent.R;
+import com.basbas.portalevent.ui.gallery.TabActivity;
 
 public class SlideshowFragment extends Fragment {
 
@@ -20,16 +22,8 @@ public class SlideshowFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        Intent intent = new Intent(getActivity(), KeranjangActivity.class);
+        startActivity(intent);
+        return null;
     }
 }
